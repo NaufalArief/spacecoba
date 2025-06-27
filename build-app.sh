b08c7165de64265bb0ed42f90505e582bce66e59
NaufalArief <naufalfdzg@gmail.com>
2025-06-27 22:42:51 +0700

    b

--- build-app.sh
+++ build-app.sh
@@ -7,11 +7,3 @@ set -e
 # Build assets using NPM
 npm run build
 
-# Clear cache
-php artisan optimize:clear
-
-# Cache the various components of the Laravel application
-php artisan config:cache
-php artisan event:cache
-php artisan route:cache
-php artisan view:cache
